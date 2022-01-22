1cd297d8356adb17c998fae452d5cfcd2bdba490
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2022-01-22 12:09:29 -0300

    First commit

---
+++ src/calculator.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './calculator.css'; //Importa o css do componente
+
+function Calculator() {
+  return (
+    <h1>Calculadora</h1>
+  );
+}
+
+export default Calculator; //Exporta toda a lógica do arquivo
